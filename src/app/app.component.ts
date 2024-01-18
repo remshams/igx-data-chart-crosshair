4c8d60b65b3b107315029928084abee06eaa71ce
Mathias Remshardt <remshams@gmail.com>
2024-01-18 07:53:10 +0100

    feat(chart): moves chart into dedicated component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,12 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
-import {
-  IgxDataChartCoreModule,
-  IgxDataChartCategoryModule,
-  IgxLegendModule,
-  IgxDataChartInteractivityModule,
-  IgxDataChartAnnotationModule,
-  IgxFinalValueLayerModule,
-  IgxCrosshairLayerModule,
-  IgxDataToolTipLayerModule,
-  IgxItemToolTipLayerModule,
-  IgxDataChartComponent,
-  IgxItemToolTipLayerComponent,
-} from 'igniteui-angular-charts';
+import { ChartComponent } from '../chart/chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    CommonModule,
-    FormsModule,
-    IgxDataChartCoreModule,
-    IgxDataChartCategoryModule,
-    IgxLegendModule,
-    IgxDataChartInteractivityModule,
-    IgxDataChartAnnotationModule,
-    IgxFinalValueLayerModule,
-    IgxCrosshairLayerModule,
-    IgxDataToolTipLayerModule,
-    IgxItemToolTipLayerModule,
-  ],
+  imports: [ChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  data = [
-    { Year: '2009', USA: 19 },
-    { Year: '2010', USA: 24 },
-    { Year: '2011', USA: 28 },
-    { Year: '2012', USA: 26 },
-    { Year: '2013', USA: 38 },
-    { Year: '2014', USA: 31 },
-    { Year: '2015', USA: 19 },
-    { Year: '2017', USA: 50 },
-    { Year: '2018', USA: 34 },
-    { Year: '2019', USA: 38 },
-  ];
-  data2 = [
-    { Year: '2009', USA: 19 },
-    { Year: '2010', USA: 24 },
-    { Year: '2011', USA: 28 },
-    { Year: '2013', USA: 38 },
-    { Year: '2014', USA: 31 },
-    { Year: '2016', USA: 40 },
-    { Year: '2019', USA: 38 },
-  ];
-}
+export class AppComponent {}
---
+++ src/chart/chart.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import {
+  IgxDataChartCoreModule,
+  IgxDataChartCategoryModule,
+  IgxLegendModule,
+  IgxDataChartInteractivityModule,
+  IgxDataChartAnnotationModule,
+  IgxFinalValueLayerModule,
+  IgxCrosshairLayerModule,
+  IgxDataToolTipLayerModule,
+  IgxItemToolTipLayerModule,
+} from 'igniteui-angular-charts';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    IgxDataChartCoreModule,
+    IgxDataChartCategoryModule,
+    IgxLegendModule,
+    IgxDataChartInteractivityModule,
+    IgxDataChartAnnotationModule,
+    IgxFinalValueLayerModule,
+    IgxCrosshairLayerModule,
+    IgxDataToolTipLayerModule,
+    IgxItemToolTipLayerModule,
+  ],
+})
+export class ChartComponent {
+  data = [
+    { Year: '2009', USA: 19 },
+    { Year: '2010', USA: 24 },
+    { Year: '2011', USA: 28 },
+    { Year: '2012', USA: 26 },
+    { Year: '2013', USA: 38 },
+    { Year: '2014', USA: 31 },
+    { Year: '2015', USA: 19 },
+    { Year: '2017', USA: 50 },
+    { Year: '2018', USA: 34 },
+    { Year: '2019', USA: 38 },
+  ];
+  data2 = [
+    { Year: '2009', USA: 19 },
+    { Year: '2010', USA: 24 },
+    { Year: '2011', USA: 28 },
+    { Year: '2013', USA: 38 },
+    { Year: '2014', USA: 31 },
+    { Year: '2016', USA: 40 },
+    { Year: '2019', USA: 38 },
+  ];
+}
